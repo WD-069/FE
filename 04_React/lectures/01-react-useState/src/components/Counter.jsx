b39e067d37b356ad4a0967af68345264c1756e62
Renke Brixel <renke.brixel@gmail.com>
2025-11-20 17:34:41 +0100

    add useState lecture part II

--- 04_React/lectures/01-react-useState/src/components/Counter.jsx
+++ 04_React/lectures/01-react-useState/src/components/Counter.jsx
@@ -1,29 +1,51 @@
 import { useState } from "react";
 
-const Counter = () => {
+// {darkTheme : boolean}
+const Counter = ({ darkTheme }) => {
   const [counter, setCounter] = useState(0);
 
-  const handleClick = () => {
-    // # Direct value Update - to overwrite the useState value
-    // setCounter(1);
-    // # Functional Update
-    setCounter((sceduledValue) => sceduledValue + 1); // 1 + 1 = 2
-    // console.log("New value of counter :", counter);
+  const handleClick = (e) => {
+    const operation = e.target.value;
+
+    if (operation === "+") {
+      setCounter((previousValue) => previousValue + 1);
+    } else if (operation === "-") {
+      if (counter <= 0) return;
+
+      setCounter((previousValue) => previousValue - 1);
+    }
   };
+
+  // const increaseCounter = () => {
+  //   setCounter((previousValue) => previousValue + 1);
+  // };
+
+  // const decreaseCounter = () => {
+  //   setCounter((previousValue) => previousValue - 1);
+  // };
+
+  const textColor = darkTheme ? "text-white" : "text-black";
 
   return (
     <>
-      <div className="flex w-36 justify-between border-2">
+      <div
+        className={`flex w-36 justify-between border-2 ${darkTheme ? "border-gray-100" : "border-black"}`}
+      >
         <button
+          value="+"
           onClick={handleClick}
-          className="w-12 cursor-pointer bg-green-400 p-4 font-bold"
+          className={
+            "w-12 cursor-pointer bg-green-400 p-4 font-bold " + textColor
+          }
         >
           +
         </button>
-        <span className="p-4">{counter}</span>
+        <span className={`p-4 ${textColor}`}>{counter}</span>
         <button
+          disabled={counter === 0 ? true : false}
+          value="-"
           onClick={handleClick}
-          className="w-12 cursor-pointer bg-red-400 p-4 font-bold"
+          className={`w-12 p-4 font-bold ${counter === 0 ? "bg-gray-400" : "cursor-pointer bg-red-400"} ${textColor}`}
         >
           -
         </button>
