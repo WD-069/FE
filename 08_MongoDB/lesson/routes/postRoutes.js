f7577a6fc1a03249f90c59d52e0b224afea628fa
Renke Brixel <renke.brixel@gmail.com>
2025-12-31 12:38:40 +0100

    Move MongoDB-lecture to Backend repository

--- 08_MongoDB/lesson/routes/postRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getAllPosts, createPost, getPostById, updatePost, deletePost } from "../controllers/postControllers.js";
-
-const postRouter = Router();
-
-postRouter.route('/').get(getAllPosts).post(createPost);
-postRouter.route('/:id').get(getPostById).put(updatePost).delete(deletePost);
-
-export default postRouter;
--- 08_MongoDB/lesson/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getAllUsers, createUser } from "../controllers/userControllers.js";
-
-const userRouter = Router();
-
-userRouter.route("/").get(getAllUsers).post(createUser);
-// userRouter.route("/:id").get().put().delete();
-
-export default userRouter;
